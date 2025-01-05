11c06c671f74c8e1ee3a8aa282f2a6886cf8e692
dong <dong@frontfidelity.com>
2025-01-06 01:40:44 +0800

    Initialize Virtual Site app , start with nvidia omniverse app template  106.5 main branch

--- premake5.lua
+++ premake5.lua
@@ -15,3 +15,6 @@ repo_build.prebuild_copy {
 }
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
+define_app("ff.virtual_site.kit")
+define_app("ff.virtual_site_streaming.kit")
+define_app("ff.virtual_site_ovc.kit")
